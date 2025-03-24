3419365233789541e4c3e87e291a639d62ec49bb
Aichaz <el.mardizarrouk@gmail.com>
2025-03-24 22:30:36 +0000

    up

--- index.js
+++ index.js
@@ -2,11 +2,7 @@ const express = require('express');
 const app = express(); 
 const db=require('./db');
 const cors = require('cors');
-app.use(cors({
-    origin: ['http://localhost:3000', 'https://intelli-annonce.vercel.app/'], // Add other origins if needed
-    methods: ['GET', 'POST', 'PUT', 'DELETE'], // Specify allowed methods
-    credentials: true,
-  }));
+app.use(cors({ origin: '*' }));
 const { createArticle ,AllArticle ,UpdateArticle,singlArticle,DeleteArticle,ArticlesByCategory} = require('./controllers/articleConteroller');
 const port = 5000; 
 app.use(express.json()); 
